9727d981987290c2ef2ba173ea277ad8d425461b
Samveen <samveen@samveen.in>
2024-03-26 22:39:02 +0530

    Fix firmware names, Acer firmware install script
    Signed-off-by: Samveen <samveen@samveen.in>

--- firmware/get-firmware.sh
+++ firmware/get-firmware.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Debug logging
+set -x
+
 # Get the driver zip listed in the URL file
 URL=acer.url
-ZIP=$(basename $(sed 's/\?.*//' < $URL))
+ZIP="$(echo -e "$(basename $(sed 's/\?.*//' < $URL) | sed 'y/+/ /; s/%/\\x/g')")"
 
-wget -O $ZIP -i $URL
+wget -O "$ZIP" -i $URL
 
-unzip $ZIP
+unzip "$ZIP"
+
+sudo mv -iv "${ZIP//A.zip}(MTK7902)"/*MT7902* /lib/firmware/mediatek/
